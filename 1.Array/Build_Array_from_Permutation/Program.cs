e22354015c74c91442aa88966d68edfb39b2c0bc
Mithun Thomas <mithun@sellowpay.com>
2024-06-25 07:46:53 +0530

    some array problems

---
+++ 1.Array/Build_Array_from_Permutation/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace BuildArrayFromPermutation
+{
+
+    public class Solution
+    {
+        public int[] BuildArray(int[] nums)
+        {
+            int[] newArray = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                newArray[i] = nums[nums[i]];
+            }
+            return newArray;
+        }
+    }
+    public static class Program
+    {
+        public static void Main()
+        {
+            int[] nums = { 0, 2, 1, 5, 3, 4 };
+            Solution solution = new Solution();
+            int[] result = solution.BuildArray(nums);
+            Console.WriteLine(string.Join(",", result));
+        }
+    }
+}
